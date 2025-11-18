9412f8fcce4960ce2445e0a6c25410bc5120bda5
0nMarvin <[200~marcusvmolinaf@gmail.comgit config --global user.email 200~marcusvmolinaf@gmail.comgit config --global user.email [200~marcusvmolinaf@gmail.com>
2025-11-18 17:23:11 -0300

    fix: vite base config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/FullStack/",
+  base: "/FullStack-PipelineTest/",
   test: {
     globals: true,
     environment: "jsdom",
